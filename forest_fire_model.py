451b716748580a23f81a78aab43761f51493b7b2
PieterO99 <pieterout99@gmail.com>
2021-12-03 14:41:51 +0100

    added tree types

--- forest_fire_model.py
+++ forest_fire_model.py
@@ -4,22 +4,23 @@ from mesa.space import MultiGrid
 from mesa.datacollection import DataCollector
 import numpy as np
 
-def compute_density(model):
-    statuses = [agent.status for agent in model.schedule.agents]
-    burning = statuses.count("burning")
-    empty = statuses.count("empty")
-    standard = statuses.count("std")
-    return (standard / (standard + empty + burning))
+# constants need to be between 0 and 1.
+burn_const_dict = {"acacia": 1, "oak": 0.9, "pine": 0.6}
+burn_time_dict = {"acacia": 15, "oak": 20, "pine": 40}
+tree_color_dict = {"acacia": "#117a65", "oak": "#229954" , "pine": "#145a32"}
+burn_color_dict = {"acacia": "#d35400", "oak": "#f1c40f" , "pine": "#a93226"}
+
 
 class TreeAgent(Agent):
     """ A tree that can burn (down)."""
-    def __init__(self, unique_id, model, status):
+    def __init__(self, unique_id, model, status, tree_type):
         super().__init__(unique_id, model)
         # standard, burning, empty
         self.status = status
+        self.tree_type = tree_type
         assert self.status in ["std", "burning", "empty"]
-        # a tree keeps burning for 5 steps until it collapses
-        self.steps_left = 20
+        
+        self.steps_left = burn_time_dict[self.tree_type]
 
     def burn(self):
         neighbours = self.model.grid.get_neighbors(self.pos, moore=True, radius=1)
@@ -32,7 +33,7 @@ class TreeAgent(Agent):
             if self.steps_left == 0:
                 self.status = "empty"
         elif self.status == "std":
-            chance_to_burn = burning_neighbours / len(neighbours)
+            chance_to_burn = burn_const_dict[self.tree_type] * burning_neighbours / len(neighbours)
             self.status = (self.random.choices(["burning", "std"], [chance_to_burn, 1-chance_to_burn], k=1))[0]
 
     def step(self):
@@ -40,7 +41,7 @@ class TreeAgent(Agent):
 
 class ForestFireModel(Model):
     """A model with some number of agents."""
-    def __init__(self, density, width, height, trees_burning):
+    def __init__(self, density, width, height, trees_burning, flora_dict):
         self.grid = MultiGrid(width, height, False)
         self.schedule = RandomActivation(self)
         self.running = True
@@ -49,21 +50,18 @@ class ForestFireModel(Model):
         tree_places = self.random.choices(range(width * height), weights=([1] * width * height), k=int(density * width * height))
         for i in range(width * height):
             if i < trees_burning:
-                a = TreeAgent(i, self, "burning")
+                flora_types = list(flora_dict)
+                flora_weights = [flora_dict[flora_type] for flora_type in flora_types]
+                a = TreeAgent(i, self, "burning", self.random.choices(flora_types, weights=flora_weights, k=1)[0])
             elif i in tree_places:
-                a = TreeAgent(i, self, "std")
+                a = TreeAgent(i, self, "std", self.random.choices(flora_types, weights=flora_weights, k=1)[0])
             else:
-                a = TreeAgent(i, self, "empty")
+                a = TreeAgent(i, self, "empty", "oak")
             self.schedule.add(a)
             # Add the agent to a random grid cell
             x = i % width
             y = i // width
             self.grid.place_agent(a, (x, y))
 
-        self.datacollector = DataCollector(
-            model_reporters={"EndDensity": compute_density},
-            agent_reporters={"Status": "status"})
-
     def step(self):
-        self.datacollector.collect(self)
         self.schedule.step()
--- forest_fire_viz.py
+++ forest_fire_viz.py
@@ -1,7 +1,8 @@
-from forest_fire_model import *
+from forest_fire_model import (tree_color_dict, burn_color_dict, ForestFireModel)
 import matplotlib.pyplot as plt
 import numpy as np
 from mesa.batchrunner import BatchRunner
+
 from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.ModularVisualization import ModularServer
 
@@ -11,14 +12,14 @@ def agent_portrayal(agent):
         portrayal = {"Shape": "rect",
                     "Filled": "true",
                     "Layer": 0,
-                    "Color": "green",
+                    "Color": tree_color_dict[agent.tree_type],
                     "w": 1,
                     "h": 1}
     elif agent.status == "burning":
         portrayal = {"Shape": "rect",
                     "Filled": "true",
                     "Layer": 0,
-                    "Color": "red",
+                    "Color": burn_color_dict[agent.tree_type],
                     "w": 1,
                     "h": 1}
     elif agent.status == "empty":
@@ -33,6 +34,7 @@ grid = CanvasGrid(agent_portrayal, 50, 50, 500, 500)
 server = ModularServer(ForestFireModel,
                        [grid],
                        "Forest Fire Model",
-                       {"density":0.7, "width":50, "height":50, "trees_burning": 50})
+                       {"density":0.7, "width":50, "height":50, "trees_burning": 50, 
+                       "flora_dict": {"acacia": 0.3, "oak": 0.6, "pine": 0.1}})
 server.port = 8521 # The default
 server.launch()
